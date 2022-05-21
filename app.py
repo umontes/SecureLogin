2d2835255fe65224ead75b1c19a541ba23930f43
umontes <umontes@ucmerced.edu>
2022-05-20 21:06:37 -0700

    fixed layout of login.html

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/home')
+def welcome():
+    return render_template('home.html')
+
+@app.route('/register')
+def create():
+    return render_template('register.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
